e5c7d8923372b79731411208d3ee894e98f1d71c
Chandan Sharma <chandan202.alive@gmail.com>
2023-03-20 18:32:00 +0100

    Changes to the queries

--- Database Scripts/SocialMedia.sql
+++ Database Scripts/SocialMedia.sql
@@ -64,7 +64,10 @@ VALUES
     (33,    'Clark Smoll'),
     (34,    'Bruys Wayne'),
     (35,    'Nicol Kirp'),
-    (36,    'Robin Tart');
+    (36,    'Robin Tart'),
+    (38,    'Eric Clapton'),
+    (39,    'Jim Morrison'),
+    (40,    'Haliey Barrey');
 
 INSERT INTO friendOf(
     person_id,
@@ -80,7 +83,11 @@ VALUES
     (7,8),
     (8,9),
     (9,10),
-    (10,11);
+    (10,11),
+    (11,12),
+    (12,13),
+    (13,14),
+    (14,15);
 
 INSERT INTO knows(
     person_id,
@@ -97,6 +104,7 @@ VALUES
     (8,19),
     (9,20),
     (10,21),
+    (11,29),
     (12,22),
     (22,23),
     (23,24),
@@ -104,6 +112,27 @@ VALUES
     (25,26),
     (14,27),
     (15,28);
+
+INSERT INTO likes(
+    person_id,
+    liked_id
+)    
+VALUES
+    (12,2),
+    (13,3),
+    (14,4)
+    (15,5),
+    (16,6),
+    (11,31),
+    (12,32),
+    (13,33),
+    (14,34),
+    (17,35),
+    (18,36),
+    (19,37),
+    (20,38),
+    (21,39),
+    (22,40);
 
 
 --ONE WAY TO DO NRE
@@ -134,13 +163,17 @@ WITH RECURSIVE cte_nre AS (
 			fo.friend_id
   	  FROM 	friendOf 	AS 	fo
 	 WHERE	fo.person_id = 3 
-	   AND	EXISTS (SELECT 1 FROM knows AS k WHERE k.person_id = fo.person_id)
+	   AND	EXISTS (SELECT 1 
+                      FROM knows AS k 
+                     WHERE k.person_id = fo.person_id)
 	 UNION
 	SELECT  fo.person_id,
 			fo.friend_id 
 	  FROM	friendOf 	AS  fo
 	  JOIN  cte_nre  	AS  c	ON c.friend_id = fo.person_id
-	 WHERE	EXISTS (SELECT 1 FROM knows AS k WHERE k.person_id = fo.person_id)
+	 WHERE	EXISTS (SELECT 1 
+                      FROM knows AS k 
+                     WHERE k.person_id = fo.person_id)
 )
 
 SELECT * FROM cte_nre;
@@ -184,13 +217,17 @@ cte_nre AS (
 			fo.friend_id
   	  FROM 	friendOf 	AS 	fo
 	 WHERE	fo.person_id = 10 
-	   AND	EXISTS (SELECT 1 FROM cte_knows AS inc WHERE fo.person_id = inc.person_id)
+	   AND	EXISTS (SELECT 1 
+                      FROM cte_knows AS inc 
+                     WHERE fo.person_id = inc.person_id)
 	 UNION
 	SELECT  fo.person_id,
 			fo.friend_id 
 	  FROM	friendOf 	AS  fo
 	  JOIN  cte_nre  	AS  c	ON c.friend_id = fo.person_id
-	 WHERE	EXISTS (SELECT 1 FROM cte_knows inc WHERE fo.person_id = inc.person_id)
+	 WHERE	EXISTS (SELECT 1 
+                      FROM cte_knows inc 
+                     WHERE fo.person_id = inc.person_id)
 )
 
 SELECT * FROM cte_nre;
@@ -227,3 +264,109 @@ cte_nre AS (
 
 SELECT * FROM cte_nre;
 
+--IN CASE WE JUST WISH TO USE NATURAL JOINS
+-- (a.[b].a)
+SELECT fof_1.fk_conn, fof_1.f_conn, fof_2.fof_target ,k.k_target
+	   FROM (SELECT fof.person_id AS fk_conn,
+	 				fof.friend_id AS f_conn
+  	 		   FROM friendOf AS fof ) AS fof_1
+	NATURAL
+	   JOIN (SELECT k.person_id AS fk_conn, 
+	 				k.known_id AS k_target
+	 		   FROM knows AS k) AS k
+	NATURAL
+	   JOIN (SELECT fof.person_id AS f_conn,
+	 				fof.friend_id AS fof_target
+  	 		   FROM friendOf AS fof ) AS fof_2
+
+--IN CASE OF JUST NATURAL JOINS
+/*
+WITH RECURSIVE cte_friendOf AS (
+	SELECT f.person_id AS source, f.friend_id AS conn
+	  FROM friendOf AS f
+   NATURAL
+	  JOIN (SELECT k.person_id AS source, k.known_id FROM knows AS k) AS k
+	 WHERE f.person_id = 3
+	 UNION 
+	SELECT f.conn AS conn, f.friend_id
+	  FROM (SELECT f.person_id AS conn, f.friend_id  FROM friendOf AS f) AS f
+   NATURAL
+	  JOIN (SELECT k.person_id AS conn, k.known_id FROM knows AS k) AS k
+   NATURAL
+	  JOIN cte_friendOf 
+)
+
+SELECT * FROM cte_friendOf
+*/
+
+--(a)+ WITH ONLY USING NATURAL JOINS
+WITH RECURSIVE cte_friendOf AS (
+	SELECT f.person_id AS source, f.friend_id AS conn
+	  FROM friendOf AS f
+	 WHERE f.person_id = 3
+	 UNION 
+	SELECT f.conn AS conn, f.friend_id
+	  FROM (SELECT f.person_id AS conn, f.friend_id  FROM friendOf AS f) AS f
+   NATURAL
+	  JOIN cte_friendOf 
+)
+
+SELECT * FROM cte_friendOf
+
+--AN ALTERNATIVE
+WITH RECURSIVE cte_friendOf AS (
+	SELECT f.person_id , f.m 
+	  FROM (SELECT person_id, friend_id AS m FROM friendOf AS f) AS f
+	 WHERE f.person_id = 3 
+	   AND EXISTS (SELECT 1 FROM knows WHERE f.person_id = person_id)
+	 UNION 
+	SELECT f.m , f.friend_id
+	  FROM (SELECT person_id AS m, friend_id  FROM friendOf AS f) AS f
+   NATURAL
+	  JOIN cte_friendOf 
+ 	 WHERE EXISTS (SELECT 1 FROM knows WHERE f.m = person_id)
+)
+
+SELECT * FROM cte_friendOf
+
+--YET ANOTHER WAY WITH THE HELP OF NATURAL JOINS
+WITH RECURSIVE cte_friendOf AS (
+	SELECT f.kws , f.m
+	  FROM (SELECT person_id AS kws, friend_id AS m FROM friendOf AS f) AS f
+   NATURAL
+	  JOIN (SELECT person_id AS kws, known_id FROM knows) AS k
+	 WHERE f.kws = 3 
+	 UNION 
+	SELECT f.m , f.friend_id
+	  FROM (SELECT person_id AS m, friend_id  FROM friendOf AS f) AS f
+   NATURAL
+	  JOIN (SELECT person_id AS m, known_id FROM knows) AS k
+   NATURAL
+	  JOIN cte_friendOf 
+ 	 
+)
+
+SELECT * FROM cte_friendOf
+
+--ACYCLIC PATTERN
+WITH RECURSIVE cte_friendOf AS (
+	SELECT f.kws , f.m
+	  FROM (SELECT person_id AS kws, friend_id AS m FROM friendOf AS f) AS f
+   NATURAL
+	  JOIN (SELECT person_id AS kws, known_id AS kn FROM knows) AS k
+   NATURAL
+	  JOIN (SELECT person_id AS kn, liked_id AS m FROM likes) AS l
+	 WHERE f.kws = 1 
+	 UNION 
+	SELECT f.m , f.fn
+	  FROM (SELECT person_id AS m, friend_id AS fn FROM friendOf AS f) AS f
+   NATURAL
+	  JOIN (SELECT person_id AS m, known_id AS kn FROM knows) AS k
+   NATURAL
+	  JOIN (SELECT person_id AS kn, liked_id AS fn FROM likes) AS l	
+   NATURAL
+	  JOIN cte_friendOf 
+ 	 
+)
+
+SELECT * FROM cte_friendOf
